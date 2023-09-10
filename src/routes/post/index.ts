68be3777619bdb623ebab143c7131b29c4a7770c
Nivin Anil <nivin@kodnest.com>
2023-09-10 12:09:07 +0530

    post completes

--- src/routes/post/index.ts
+++ src/routes/post/index.ts
@@ -43,18 +43,34 @@ postRoute.post("/post", async (req, res) => {
 postRoute.get("/post/my", auth, async (req: any, res) => {
   try {
     const { page = 0, size = 10 } = req.query;
-    const posts = await prisma.post.findMany({
+    let posts: any[] = await prisma.post.findMany({
       where: {
         authorId: req.user.id,
       },
       skip: Number(page) * Number(size),
       take: Number(size),
+      select: {
+        id: true,
+        title: true,
+        content: true,
+        published: true,
+        likedBy: {
+          select: {
+            id: true,
+          },
+        },
+      },
+    });
+    // add total likes for each post
+    posts.forEach((post) => {
+      post["totalLikes"] = post.likedBy.length;
     });
     const total = await prisma.post.count({
       where: {
         authorId: req.user.id,
       },
     });
+
     response(res, { posts, total }, null, "Get all post success", 200);
   } catch (error: any) {
     response(res, null, error.message, "Get all post failed", 400);
@@ -75,6 +91,53 @@ postRoute.get("/post/:id", auth, async (req, res) => {
     }
   } catch (error: any) {
     response(res, null, error.message, "Get post failed", 400);
+  }
+});
+
+// like togle post
+postRoute.post("/post/like/:id", auth, async (req: any, res) => {
+  try {
+    const { id } = req.params;
+    const post = await prisma.post.findUnique({
+      where: { id: id },
+      select: {
+        likedBy: true,
+      },
+    });
+    if (!post) {
+      response(res, null, "Post not found", "Get post failed", 400);
+    } else {
+      // if liked by have user id then remove it
+      if (post.likedBy.map((like) => like.id).includes(req.user.id)) {
+        await prisma.post.update({
+          where: { id: id },
+          data: {
+            likedBy: {
+              disconnect: {
+                id: req.user.id,
+              },
+            },
+          },
+        });
+        response(res, "LIKED", null, "Like post success", 200);
+      }
+      // if liked by not have user id then add it
+      else {
+        await prisma.post.update({
+          where: { id: id },
+          data: {
+            likedBy: {
+              connect: {
+                id: req.user.id,
+              },
+            },
+          },
+        });
+        response(res, "UNLIKE", null, "unLike post success", 200);
+      }
+    }
+  } catch (error: any) {
+    response(res, null, error.message, "Like post failed", 400);
   }
 });
 
